fa01ec4b9f0757130cae354daa0d3d8772aadd3c
André Fernandez <andrefteodoro@gmail.com>
2020-04-03 05:49:29 -0300

    first

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://ant.donttouch.com.br',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistasComponent } from './artistas/artistas.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FacaParteComponent } from './faca-parte/faca-parte.component';
+import { UnidadesComponent } from './unidades/unidades.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DonttouchService} from './donttouch.service';
+import {HttpClientModule} from '@angular/common/http';
+import { TimeslotComponent } from './timeslot/timeslot.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistasComponent,
+    ContatoComponent,
+    FacaParteComponent,
+    UnidadesComponent,
+    TimeslotComponent,
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        ToastrModule.forRoot({
+            timeOut: 1000,
+            progressBar: true,
+            progressAnimation: 'increasing'
+        }),
+        ReactiveFormsModule
+    ],
+  providers: [DonttouchService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
